c72b627b63625bce539d6f8c48ad704eb6f6742f
Kennedy <Richard_Kennedy@cmc-mo.com>
2020-10-30 20:17:54 -0500

    Add project files.

---
+++ Visual CSharp Step By Step/Chapter 1/TestHello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TestHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /* wow i did not realize that you could comment like this
+             
+
+
+            &
+             
+             
+             
+            */
+
+        }
+    }
+}
